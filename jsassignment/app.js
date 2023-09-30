7cacd4d7045afc04ae9e35138157dea65197c915
nehasii <nehasingh24052001@gmail.com>
2023-09-30 19:45:11 +0530

    fix: alignment

--- jsassignment/app.js
+++ jsassignment/app.js
@@ -10,17 +10,24 @@ function toggleShow() {
 
 let toggleState = false;
 
+// function toggleHideShow() {
+//   const tglbtn = document.getElementById("tglbtn");
+//   const image2 = document.getElementById("image2");
+//   if (toggleState === true) {
+//     tglbtn.innerText = "show";
+//     image2.style.visibility = "hidden";
+//     toggleState = false;
+//   } else {
+//     tglbtn.innerText = "hide";
+//     image2.style.visibility = "visible";
+//     toggleState = true;
+//   }
+// }
+
 function toggleHideShow() {
   const tglbtn = document.getElementById("tglbtn");
   const image2 = document.getElementById("image2");
-  if (toggleState === true) {
-    tglbtn.innerText = "show";
-    image2.style.visibility = "hidden";
-    toggleState = false;
-  } else {
-    tglbtn.innerText = "hide";
-    image2.style.visibility = "visible";
-    toggleState = true;
-  }
+  image2.style.visibility = toggleState ? "hidden" : "visible";
+  tglbtn.innerText = toggleState ? "show" : "hide";
+  toggleState = toggleState ? false : true;
 }
-
