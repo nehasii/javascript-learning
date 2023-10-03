7ee1dc643934e08e0cf34d3029533f7a231bda55
nehasii <nehasingh24052001@gmail.com>
2023-10-03 23:23:55 +0530

    feat: products fetching through api

---
+++ jsassignment/apiass.js
@@ -0,0 +1,36 @@
+fetch('https://fakestoreapi.com/products')
+.then(response => {
+    return response.json();
+})
+.then(data => {
+    const productsDiv = document.getElementById('products');
+    data.forEach(product => {
+        const productDiv = document.createElement('div');
+        productDiv.className = 'product';
+
+        const productTitle = document.createElement('h2');
+        productTitle.textContent = product.title;
+        productDiv.appendChild(productTitle);
+
+        const productImg = document.createElement('img');
+        productImg.src = product.image;
+        productDiv.appendChild(productImg);
+
+        const productPrice = document.createElement('p');
+        productPrice.textContent = `Price: Rs.${product.price}`;
+        productDiv.appendChild(productPrice);
+
+        const productDescription = document.createElement('p');
+        productDescription.textContent = product.description;
+        productDiv.appendChild(productDescription);
+
+        const productRating = document.createElement('p');
+        productRating.textContent = `Rating: ${product.rating.rate} (${product.rating.count} reviews)`;
+        productDiv.appendChild(productRating);
+
+        productsDiv.appendChild(productDiv);
+    });
+})
+.catch(error => {
+    console.error('There was a problem with the fetch operation:', error.message);
+});
